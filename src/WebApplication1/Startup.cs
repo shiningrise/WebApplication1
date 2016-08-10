fb948524c5dad987a2f18c987f9fe6e00ff6efb0
吴晓阳 <shiningrise@gmail.com>
2016-08-10 22:56:17 +0800

    11

--- src/WebApplication1/Startup.cs
+++ src/WebApplication1/Startup.cs
@@ -22,7 +22,7 @@ namespace WebApplication1
         {
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World! test");
+                await context.Response.WriteAsync("Hello World! test11");
             });
         }
     }
